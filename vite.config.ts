ff424868630a9b7d26a71b9b6b96c8fa71b2a521
mikesaintsg <45922630+mikesaintsg@users.noreply.github.com>
2026-01-13 06:34:20 -0500

    Change library name from 'typebuilder' to 'indexeddb'

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
-			name: 'typebuilder',
+			name: 'indexeddb',
 			fileName: 'index',
 			formats: ['es'],
 		},
